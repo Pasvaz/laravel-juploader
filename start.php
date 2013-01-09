cec1d5f512ab4a749558583cf7642bd6fef164fa
Pasvaz <pasqualevazzana@gmail.com>
2013-01-09 04:32:52 +0100

    Created an asset container for debugs

--- start.php
+++ start.php
@@ -27,29 +27,32 @@ Laravel\IoC::register('Uploader', function() use ($options)
 Asset::container('jupload')
     ->bundle('jupload')
 
-    ->add('fileupload-ui-css',	'css/jquery.fileupload-ui.css')
-    ->add('style',         		'css/style.css')
-
-    ->add('jquery-ui',			'js/vendor/jquery.ui.widget.js')
-    ->add('tmpl',				'js/extra/tmpl.min.js')
-    //DEBUG ->add('tmpl',		'js/extra/tmpl.js')
-    ->add('load-image',			'js/extra/load-image.min.js')
-    //DEBUG ->add('load-image',	'js/extra/load-image.js')
-    ->add('iframe-transport',	'js/jquery.iframe-transport.js')
-    ->add('fileupload',			'js/jquery.fileupload.js')
-    ->add('fileupload-fp',      'js/jquery.fileupload-fp.js')
-    ->add('fileupload-ui',		'js/jquery.fileupload-ui.js')
-    ->add('main',				'js/main.js')
-    ->add('locale',				'js/locale.js');
+    ->add('fileupload-ui-css',		'css/jquery.fileupload-ui.css')
+    ->add('style',         			'css/style.css')
+	
+    ->add('jquery-ui',				'js/vendor/jquery.ui.widget.js')
+    ->add('tmpl',					'js/extra/tmpl.min.js')
+    ->add('load-image',				'js/extra/load-image.min.js')
+    ->add('iframe-transport',		'js/jquery.iframe-transport.js')
+    ->add('fileupload',				'js/jquery.fileupload.js')
+    ->add('fileupload-fp',      	'js/jquery.fileupload-fp.js')
+    ->add('fileupload-ui',			'js/jquery.fileupload-ui.js')
+    ->add('main',					'js/main.js')
+    ->add('locale',					'js/locale.js');
 
 Asset::container('jupload-gallery')
     ->bundle('jupload')
-    //->add('bootstrap-gallery-css',	'css/extra/bootstrap-image-gallery.css')
-    ->add('bootstrap-gallery-css',		'css/extra/bootstrap-image-gallery.min.css')
-    //->add('bootstrap-gallery',		'js/extra/bootstrap-image-gallery.js');
-    ->add('bootstrap-gallery',			'js/extra/bootstrap-image-gallery.min.js');
+    ->add('bootstrap-gallery-css',	'css/extra/bootstrap-image-gallery.min.css')
+    ->add('bootstrap-gallery',		'js/extra/bootstrap-image-gallery.min.js');
 
 Asset::container('jupload-optional')
     ->bundle('jupload')
-    //->add('canvas-to-blob',			'js/extra/canvas-to-blob.js');
-    ->add('canvas-to-blob',				'js/extra/canvas-to-blob.min.js');
+    ->add('canvas-to-blob',			'js/extra/canvas-to-blob.min.js');
+
+Asset::container('jupload-debug')
+    ->bundle('jupload')
+    ->add('tmpl',					'js/extra/tmpl.js')
+    ->add('load-image',				'js/extra/load-image.js')
+    ->add('bootstrap-gallery-css',	'css/extra/bootstrap-image-gallery.css')
+    ->add('bootstrap-gallery',		'js/extra/bootstrap-image-gallery.js')
+    ->add('canvas-to-blob',			'js/extra/canvas-to-blob.js');
